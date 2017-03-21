ef32cf2afe5cc27558831bea601d03b030f33fbe
Dima Lytvyniuk <wppresent@outlook.com>
2017-03-21 21:01:49 +0200

    Laba_2: Finish

---
+++ Laba_2/Laba_2/ControlClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Laba_2
+{
+    class ControlClass
+    {
+        public void OpenGenWindow()
+        {
+            Window_Gen win_1 = new Window_Gen();
+
+            if (win_1.ShowDialog() == true)
+            {
+                win_1.WindowState = WindowState.Normal;
+
+                if (win_1.DialogResult == true)
+                {
+
+                }
+                else
+                    Environment.Exit(0);
+            }
+            else
+                Environment.Exit(0);
+        }
+
+        public void OpenHandWindow()
+        {
+            Window_Hand win_1 = new Window_Hand();
+
+            if (win_1.ShowDialog() == true)
+            {
+                win_1.WindowState = WindowState.Normal;
+
+                if (win_1.DialogResult == true)
+                {
+
+                }
+                else
+                    Environment.Exit(0);
+            }
+            else
+                Environment.Exit(0);
+        }
+    }
+}
---
+++ Laba_2/Laba_2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ControlClass controlClass;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            controlClass = new ControlClass();
+        }
+
+        private void button_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button_Gen_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            controlClass.OpenGenWindow();
+            this.Visibility = Visibility.Visible;
+        }
+
+        private void button_Hand_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            controlClass.OpenHandWindow();
+            this.Visibility = Visibility.Visible;
+        }
+    }
+}
